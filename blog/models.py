1bbee19172d010c71ff3e37eea8bc24edf777242
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-06-30 15:04:02 +0000

    Add blog template

--- blog/models.py
+++ blog/models.py
@@ -28,4 +28,4 @@ class Comment(models.Model):
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True) 
