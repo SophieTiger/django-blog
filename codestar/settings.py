e7fbc5c4d4e7cabf225de02e8b1ab7e26e3f63eb
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-07-02 07:59:02 +0000

    Add about app and about model

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-sophietiger-djangoblog-iz8j9zv95u9.ws.codeinstitute-ide.net', '.herokuapp.com']
 
