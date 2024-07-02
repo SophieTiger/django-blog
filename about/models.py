2fa714a28a3a8b0ae67f04cd276a6451fa8fc991
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-07-02 13:45:11 +0000

    Add about template and urls

--- about/models.py
+++ about/models.py
@@ -5,4 +5,6 @@ class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
 
